4ecc5bc85e68d27cc11a28e6b235bafa883b6925
Nicholas <nickycrandall@gmail.com>
2021-10-04 15:29:48 -0400

    added controller and model directories

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT
 
+//mongoose
+mongoose.connect(process.env.MONGODBURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+})
+
+const db = mongoose.connection;
+db.once('open', ()=> console.log('DB connected...'));
+db.on('error', (error)=> console.log(error.message));
+db.on('disconnected', ()=> console.log('Mongoose disconnected...'));
+
+//controllers
+app.use('/blog', require('./controllers/blogController'))
+
 app.listen(PORT, () => {
     console.log('server is listening')
 })
